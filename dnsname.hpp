11f4ec73e59eb190a55fa696377bc8023188d729
Andrew Nelless <andrew@nelless.net>
2015-06-18 17:24:25 +0100

    Add iterator skeleton

--- dnsname.hpp
+++ dnsname.hpp
@@ -1,4 +1,24 @@
 #include <string>
+#include <boost/utility/string_ref.hpp> 
+#include <boost/iterator/iterator_facade.hpp>
+
+using DNSLabel = boost::string_ref;
+
+class DNSLabelIterator final: public boost::iterator_facade<
+    DNSLabelIterator, 
+    DNSLabel,
+    boost::iterators::bidirectional_traversal_tag,
+    DNSLabel
+>{
+    public:
+        DNSLabel dereference() const noexcept;
+        bool equal (DNSLabelIterator const&) const noexcept;
+        void increment() noexcept;
+        void decrement() noexcept;
+    private:
+        std::string::const_iterator str_it_;
+        char llen_;
+};
 
 class DNSName final {
     public:
